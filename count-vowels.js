0fb8fe677a04df712ad31a6215e8b32ec8c43cb2
Harper <HarperMelissaM@gmail.com>
2025-04-29 15:30:28 -0500

    updated countVowels function

--- count-vowels.js
+++ count-vowels.js
@@ -1,11 +1,12 @@
 // Function takes a string and returns the number of vowels (a, e, i, o, u) in the string.
 function countVowels(string) {
     let count = 0;
-    for(let i= 0; i < string.length; i++) {
-        if (string[i] === 'a'||string[i] === 'e'||string[i] === 'i'||string[i] === 'o'||string[i] === 'u'){
-            count += 1;
-        };
-    } 
+    let allowed = "aeiou"
+    for(char of string) {
+        if(allowed.includes(char)) {
+            count += 1
+        }
+    }
     return count;
 }
 
